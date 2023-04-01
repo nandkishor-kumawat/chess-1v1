8076a6f4584d67b8281ad0832b9760cbdf6cd442
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2023-04-02 02:54:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
